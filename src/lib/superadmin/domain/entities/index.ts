3337ef7ce3de5649a5391193d882ce77ad7d307b
angelchavez19 <infoangelchavez@gmail.com>
2025-10-22 10:09:09 -0500

    feat(superadmin): superadmin repository and canCreateSuperUser permission

---
+++ src/lib/superadmin/domain/entities/index.ts
@@ -0,0 +1,2 @@
+export * from "./superadmin/superadmin";
+export * from "./superadmin/value-objects";
---
+++ src/lib/superadmin/domain/entities/superadmin/value-objects/superadmin-can-create-superusers.ts
@@ -0,0 +1,3 @@
+import { BooleanValueObject } from "~/lib/Shared/domain";
+
+export class SuperAdminCanCreateSuperUsers extends BooleanValueObject {}
