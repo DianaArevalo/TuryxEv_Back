4a68d016ad8cc6adfe2b4158bd5c942b96c1c832
DianaArevalo <morec9834@gmail.com>
2025-10-24 21:25:58 -0500

    HOTEL Infraestructure_repository_sinmetodos en Location

---
+++ src/lib/Hotel/infraestructure/repositories/MongoLocationRepository.ts
@@ -0,0 +1,19 @@
+import { CityRepository, Hotel, HotelLocation } from "../../domain";
+
+export class MongoLocationRepository implements CityRepository {
+    getValidCities(): Promise<HotelLocation[]> {
+         throw new Error("Method not implemented.");
+    }
+
+    isValidCity(city: HotelLocation): Promise<boolean> {
+         throw new Error("Method not implemented.");
+    }
+
+    getHotelsByCity(): Promise<Hotel[]> {
+         throw new Error("Method not implemented.");
+    }
+
+    createCity(city: string): Promise<HotelLocation> {
+         throw new Error("Method not implemented.");
+    }
+}
