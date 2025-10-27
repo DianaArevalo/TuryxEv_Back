6899ee906b86b2b5d8f255fe2cff8fc6c82f835e
angelchavez19 <infoangelchavez@gmail.com>
2025-10-27 17:26:19 -0500

    feat(superadmin): express router

---
+++ src/lib/superadmin/infrastructure/routers/express.ts
@@ -0,0 +1,30 @@
+import { express as ex } from "../../../Shared/Infraestructure/External";
+import { ExpressSuperAdminController } from "../controllers/express";
+
+const controller = new ExpressSuperAdminController();
+const ExpressSuperAdminRouter = ex.Router();
+
+ExpressSuperAdminRouter.post("/create", controller.create);
+ExpressSuperAdminRouter.patch("/edit", controller.edit);
+ExpressSuperAdminRouter.get("/get-all", controller.getAll);
+ExpressSuperAdminRouter.get(
+  "/get-all-by-isactive",
+  controller.getAllByIsActive
+);
+ExpressSuperAdminRouter.get("/get-one-by-email", controller.getOneByEmail);
+ExpressSuperAdminRouter.get("/get-one-by-id", controller.getOneById);
+ExpressSuperAdminRouter.get("/business/edit", controller.editBusiness);
+ExpressSuperAdminRouter.get(
+  "/reservations/get-all-by-hotel-id",
+  controller.getAllReservationsByHotelId
+);
+ExpressSuperAdminRouter.get(
+  "/reservations/get-all-user-reservations",
+  controller.getAllUserReservations
+);
+ExpressSuperAdminRouter.get(
+  "/reservations/get-one-reservation",
+  controller.getOneReservation
+);
+
+export { ExpressSuperAdminRouter };
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
-
 import { express as ex, cors } from "./lib/Shared/Infraestructure/External";
 import { connectMongo } from "./lib/db/mongoose";
 import { ExpressUserRouter } from "./lib/User/infrastructure/routers/ExpressUserRouter";
-import {config} from "./config/config"
+import { config } from "./config/config";
 import { ExpressReservationRouter } from "./lib/Reservation/infrastructure/routers/express";
-import { HttpError } from "./lib/Shared/domain/exeptions";
 
 //import { ExpressHotelRouter } from "./lib/Hotel/infraestructure/routers/expressHotelRouter";
 
 import { ExpressBusinessRouter } from "./lib/bussiness/infrastructure/routers/express";
+import { HttpError } from "./lib/Shared/domain/exeptions";
+import { ExpressSuperAdminRouter } from "./lib/superadmin/infrastructure/routers/express";
 import { ExpressHotelRouter } from "./lib/Hotel/infraestructure/routers/ExpressHotelRouter";
 import { ApiResponse } from "./lib/Shared/Infraestructure/ApiResponse";
-
-
-
 
 const app = ex();
 
@@ -22,48 +19,43 @@ const app = ex();
 app.use(cors({ origin: config.mongoUri, credentials: true }));
 app.use(ex.json());
 
-
 // Rutas
-app.use('/api/business', ExpressBusinessRouter);
+app.use("/api/business", ExpressBusinessRouter);
 app.use("/api/users", ExpressUserRouter);
-app.use('/api/reservations', ExpressReservationRouter);
-app.use('/api/hotel', ExpressHotelRouter)
+app.use("/api/reservations", ExpressReservationRouter);
+app.use("/api/hotel", ExpressHotelRouter);
+app.use("/api/superadmin", ExpressSuperAdminRouter);
 
 // Middleware de errores
-app.use((
-    err: unknown,
-    req: ex.Request,
-    res: ex.Response,
-    next: ex.NextFunction
-) => {
-    if(err instanceof HttpError) {
-        const response: ApiResponse<null> = {
-            success: false,
-            title: "Ocurrio un error",
-            message: err.message,
-            body: null,
-        };
+app.use(
+  (err: unknown, req: ex.Request, res: ex.Response, next: ex.NextFunction) => {
+    if (err instanceof HttpError) {
+      const response: ApiResponse<null> = {
+        success: false,
+        title: "Ocurrio un error",
+        message: err.message,
+        body: null,
+      };
 
-        return res.status(err.statusCode).json(response);
+      return res.status(err.statusCode).json(response);
     }
 
     if (err instanceof Error) {
-         console.error(err.stack);
-        return res.status(500).json(err.message);        
+      console.error(err.stack);
+      return res.status(500).json(err.message);
     }
     console.error(err);
-    return res.status(500).json("Something wrong!");    
-});
+    return res.status(500).json("Something wrong!");
+  }
+);
 
 // Conectar DB y levantar servidor
 connectMongo(config.mongoUri)
-    .then(() => {
-        app.listen(config.port, () => {
-            console.log(`✅ Server is running on http://localhost:${config.port}`);
-        });
-    })
-    .catch((err) => {
-        console.error("❌ Error connecting to MongoDB:", err);
+  .then(() => {
+    app.listen(config.port, () => {
+      console.log(`✅ Server is running on http://localhost:${config.port}`);
     });
-
-
+  })
+  .catch((err) => {
+    console.error("❌ Error connecting to MongoDB:", err);
+  });
