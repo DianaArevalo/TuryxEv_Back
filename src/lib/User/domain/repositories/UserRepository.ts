00417dae8ea270eedd0b39a2206de3d60916677a
DianaArevalo <morec9834@gmail.com>
2025-11-02 22:41:11 -0500

    repositorio

--- src/lib/User/domain/repositories/UserRepository.ts
+++ src/lib/User/domain/repositories/UserRepository.ts
@@ -1,14 +1,15 @@
+import { User } from "../entities/User/User";
+import { UserEmail, UserId, UserStatus } from "../entities/User/value-objects";
 
-import {User} from "./User"
-import { UserEmail } from "./UserEmail"
-import { UserId } from "./UserId"
+
 
 export interface UserRepository {
     create(user: User): Promise<void>
     getOneById(id: UserId): Promise<User | null>
     getAll(): Promise<User[]>;
-    //getOneByEmail(email: UserEmail): Promise <User | null>
+    getOneByEmail(email: UserEmail): Promise <User | null>
     edit(user: User): Promise<void>
-    delete(id: UserId): Promise<void>
+    softDelete(id: UserId): Promise<void>
+    getAllByIsActive(isActive: UserStatus): Promise<User[]>
 }
 
