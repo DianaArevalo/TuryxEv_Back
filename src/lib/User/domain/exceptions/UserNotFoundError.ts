88071b29b65a035a5b6b559710d8b57304634034
DianaArevalo <morec9834@gmail.com>
2025-11-02 22:44:07 -0500

    exceptions

--- src/lib/User/domain/exceptions/UserNotFoundError.ts
+++ src/lib/User/domain/exceptions/UserNotFoundError.ts
@@ -1,1 +1,7 @@
-export class UserNotFoundError extends Error {}
+import { NotFoundError } from "~/lib/Shared/domain";
+
+export class UserNotFoundError extends NotFoundError {
+    constructor(message: string = "User not found"){
+        super(message);
+    }
+}
