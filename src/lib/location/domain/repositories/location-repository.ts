7382c55b5e119f4df2d74f455c860727051514ce
angelchavez19 <infoangelchavez@gmail.com>
2025-11-04 11:37:34 -0500

    feat(location): create city, get location by owner, get valid cities and update location

--- src/lib/location/domain/repositories/location-repository.ts
+++ src/lib/location/domain/repositories/location-repository.ts
@@ -4,13 +4,16 @@ import {
   Location,
   LocationBusinessId,
   LocationHotelId,
+  LocationId,
 } from "../entities";
 
 export interface LocationRepository {
-  getValidLocations(): Promise<Location[]>;
-  isValidLocation(location: Location): Promise<boolean>;
+  getValidCities(): Promise<City[]>;
+  isValidCity(cityName: CityName): Promise<boolean>;
   getLocationByHotel(hotel: LocationHotelId): Promise<Location>;
   getLocationByBusiness(business: LocationBusinessId): Promise<Location>;
+  getOneLocation(locationId: LocationId): Promise<Location>;
+  getOneCityByName(cityName: CityName): Promise<City>;
   create(location: Location): Promise<Location>;
   createCity(cityName: CityName): Promise<City>;
   update(location: Location): Promise<Location>;
