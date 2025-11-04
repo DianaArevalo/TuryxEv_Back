c53224ae823e53425cbcab6da0fe2e2918cb13a3
DianaArevalo <morec9834@gmail.com>
2025-11-04 18:42:52 -0500

    rutas

--- src/lib/User/infrastructure/routers/ExpressUserRouter.ts
+++ src/lib/User/infrastructure/routers/ExpressUserRouter.ts
@@ -1,14 +1,14 @@
-import { express as ex } from "../../Shared/Infraestructure/External";
-import { ExpressUserController } from "./controllers/ExpressUserController";
+import { ExpressUserController } from "../controllers/ExpressUserController";
+import {express as ex} from "../../../Shared/Infraestructure/External"
 
 const controller = new ExpressUserController();
-
 const ExpressUserRouter = ex.Router();
 
-ExpressUserRouter.get("/users/", controller.getAll);
-ExpressUserRouter.get("/user/:id", controller.getOneById);
-ExpressUserRouter.post("/user/", controller.create);
-ExpressUserRouter.put("/user/", controller.edit);
-ExpressUserRouter.delete("/user/:id", controller.delete);
+ExpressUserRouter.post("/create-user", controller.create);
+ExpressUserRouter.get("/get-user-one-by-id", controller.getOneById);
+ExpressUserRouter.get("/get-all-users", controller.getAll);
+ExpressUserRouter.get("/get-user-by-email", controller.getOneByEmail);
+ExpressUserRouter.patch("/edit-user", controller.edit);
+ExpressUserRouter.patch("/user/:id", controller.softDelete);
 
 export { ExpressUserRouter };
