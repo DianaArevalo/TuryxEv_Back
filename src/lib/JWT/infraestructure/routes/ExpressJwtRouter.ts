6ce2efe82b3bc0d0c82362d7c6ed2fa04410bd1f
DianaArevalo <morec9834@gmail.com>
2025-11-25 21:33:09 -0500

    middleware I forgot it

--- src/lib/JWT/infraestructure/routes/ExpressJwtRouter.ts
+++ src/lib/JWT/infraestructure/routes/ExpressJwtRouter.ts
@@ -1,11 +1,12 @@
 import { JwtHttpController } from "../controller/JwtHttpController";
 import { express as ex } from "../../../Shared/Infraestructure/External";
+import { verifyAccessTokenMiddleware } from "../middlewares/VerifyAccessTokenMiddleware";
 
 const controller = new JwtHttpController();
 const ExpressJwtRouter = ex.Router();
 
 ExpressJwtRouter.post("/jwt/sign", controller.jwtSign);
 ExpressJwtRouter.post("/jwt/refresh", controller.jwtRefresh);
-ExpressJwtRouter.post("/jwt/revoke", controller.jwtRevoke);
+ExpressJwtRouter.post("/jwt/revoke", verifyAccessTokenMiddleware, controller.jwtRevoke);
 
 export { ExpressJwtRouter };
